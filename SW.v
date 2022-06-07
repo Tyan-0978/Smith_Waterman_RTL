b5f378213192c8274e65038b65493ea73119bc07
Nathan Tyan <960007nathan@gmail.com>
2022-06-07 11:22:20 +0800

    finish debugging, RTL simulation passed

--- SW.v
+++ SW.v
@@ -26,10 +26,10 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
     parameter CONT  = 2'b10;
 
     // constants
-    parameter MATCH    = 3'b010;
-    parameter MISMATCH = 3'b111;
-    parameter G_OPEN   = 4'b0010;
-    parameter G_EXTEND = 5'b00001;
+    parameter MATCH    = 2;
+    parameter MISMATCH = -1;
+    parameter G_OPEN   = 2;
+    parameter G_EXTEND = 1;
 
 //------------------------------------------------------------------
 // reg & wire
@@ -63,37 +63,41 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
     reg        comp_state, next_comp_state; // comparison state
 
     // indices -------------------------------------------
-    reg  [5:0] i [0:15];
-    reg  [5:0] j [0:15];
-    reg  [5:0] delay_i [0:15];
-    reg  [5:0] delay_j [0:15];
-    reg  [5:0] next_i [0:15];
-    reg  [5:0] next_j [0:15];
+    reg  [WIDTH_POS_REF  -1:0] i [0:15];
+    reg  [WIDTH_POS_QUERY-1:0] j [0:15];
+    reg  [WIDTH_POS_REF  -1:0] delay_i [0:15];
+    reg  [WIDTH_POS_QUERY-1:0] delay_j [0:15];
+    reg  [WIDTH_POS_REF  -1:0] next_i [0:15];
+    reg  [WIDTH_POS_QUERY-1:0] next_j [0:15];
     reg  [5:0] cyc_count, next_cyc_count;
 
     // tables --------------------------------------------
-    reg  signed [2:0] match [0:63][0:47];
-    reg  signed [3:0] ins   [0:64][0:48];
-    reg  signed [3:0] del   [0:64][0:48];
-    reg  signed [4:0] high  [0:64][0:48];
+    reg  signed [7:0]             match [0:63][0:47];
+    reg  signed [WIDTH_SCORE-1:0] ins   [0:64][0:48];
+    reg  signed [WIDTH_SCORE-1:0] del   [0:64][0:48];
+    reg  signed [WIDTH_SCORE-1:0] high  [0:64][0:48];
 
     // new cell values -----------------------------------
-    wire signed [2:0] next_match [0:15];
-    wire signed [3:0] next_ins   [0:15];
-    wire signed [3:0] next_del   [0:15];
-    reg  signed [4:0] next_high  [0:15];
+    wire signed [2:0]             next_match [0:15];
+    wire signed [WIDTH_SCORE-1:0] next_ins   [0:15];
+    wire signed [WIDTH_SCORE-1:0] next_del   [0:15];
+    reg  signed [WIDTH_SCORE-1:0] next_high  [0:15];
 
     // values for comparisons ----------------------------
-    wire signed [3:0] high_ins_open [0:15];
-    wire signed [3:0] high_del_open [0:15];
-    wire signed [3:0] ins_ext       [0:15];
-    wire signed [3:0] del_ext       [0:15];
-    wire signed [4:0] high_match    [0:15];
+    wire signed [WIDTH_SCORE-1:0] high_ins_open [0:15];
+    wire signed [WIDTH_SCORE-1:0] high_del_open [0:15];
+    wire signed [WIDTH_SCORE-1:0] ins_ext       [0:15];
+    wire signed [WIDTH_SCORE-1:0] del_ext       [0:15];
+    wire signed [WIDTH_SCORE-1:0] high_match    [0:15];
 
     // for loops indices
+    genvar u;
     integer x, y, z;
 
-    // for debugging
+    // signals for debugging
+    wire signed [7:0] debug_ins_delay;
+    wire signed [7:0] debug_del_delay;
+    wire signed [7:0] debug_high_dij;
 
 //------------------------------------------------------------------
 // submodule
@@ -103,21 +107,35 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 // combinational part
 //------------------------------------------------------------------
 
+    // debug
+    assign debug_ins_delay = ins[3][18];
+    assign debug_del_delay = del[3][18];
+    assign debug_high_dij = high[2][17];
+
     // compute values for comparisons --------------------
-    for (integer x = 0; x <= 15; x = x + 1) begin
-	assign high_ins_open[x] = high[i[x]  ][j[x]-1] - G_OPEN;
-	assign high_del_open[x] = high[i[x]-1][j[x]  ] - G_OPEN;
-	assign ins_ext[x]       = ins [i[x]  ][j[x]-1] - G_EXTEND;
-	assign del_ext[x]       = del [i[x]-1][j[x]  ] - G_EXTEND;
-	assign high_match[x] = high[delay_i[x]-1][delay_j[x]-1] + match[delay_i[x]-1][delay_j[x]-1];
+    for (u = 0; u <= 15; u = u + 1) begin
+        // highest cell value is computed in the next cycle,
+	// so in some cases I use the current next_high cell value
+        if (u == 0)
+	    // first PE: use upper highest cell value
+	    assign high_ins_open[u] = high[i[u]][j[u]-1] - G_OPEN;
+	else
+	    // other PE: use next_high cell value from previous PE
+	    assign high_ins_open[u] = next_high[u-1] - G_OPEN;
+
+	assign high_del_open[u] = (i[u] == 1) ? - G_OPEN : next_high[u] - G_OPEN;
+
+	assign ins_ext[u] = ins [i[u]  ][j[u]-1] - G_EXTEND;
+	assign del_ext[u] = del [i[u]-1][j[u]  ] - G_EXTEND;
+	assign high_match[u] = high[delay_i[u]-1][delay_j[u]-1] + match[delay_i[u]-1][delay_j[u]-1];
     end
 
     // match, ins, del cell logic
     // highest cell logic is in the always block for multiple comparisons
-    for (integer x = 0; x <= 15; x = x + 1) begin
-        assign next_match[x] = (ref[i[x]-1] == query[j[x]-1]) ? MATCH : MISMATCH;
-	assign next_ins  [x] = (high_ins_open[x] >= ins_ext[x]) ? high_ins_open[x] : ins_ext[x];
-	assign next_del  [x] = (high_del_open[x] >= del_ext[x]) ? high_del_open[x] : del_ext[x];
+    for (u = 0; u <= 15; u = u + 1) begin
+        assign next_match[u] = (ref[i[u]-1] == query[j[u]-1]) ? MATCH : MISMATCH;
+	assign next_ins  [u] = (high_ins_open[u] >= ins_ext[u]) ? high_ins_open[u] : ins_ext[u];
+	assign next_del  [u] = (high_del_open[u] >= del_ext[u]) ? high_del_open[u] : del_ext[u];
     end
 
     // output --------------------------------------------
@@ -127,14 +145,14 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
         // initialize ins, del, high tables --------------
 	// row 0
         for (x = 1; x <= 64; x = x + 1) begin
-            ins[x][0] = 4'b1001; // -7
-            del[x][0] = 4'b1001;
+            ins[x][0] = -8;
+            del[x][0] = -8;
             high[x][0] = 0;
         end
 	// column 0
         for (y = 0; y <= 48; y = y + 1) begin
-            ins[0][y] = 4'b1001;
-            del[0][y] = 4'b1001;
+            ins[0][y] = -8;
+            del[0][y] = -8;
             high[0][y] = 0;
         end
 
@@ -154,7 +172,7 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 		    next_fill_state = fill_state;
 	    end
 	    CONT: begin
-	        if (delay_i[15] == 64 && delay_j[15] == 33) // filling finished
+	        if (delay_i[15] == 64 && delay_j[15] == 48) // filling finished
 		    next_fill_state = WAIT;
 		else
 		    next_fill_state = fill_state;
@@ -188,8 +206,8 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 		        next_i[x] = i[x] + 1;
 		    else
 		        next_i[x] = 0;
+		    next_j[x] = j[x];
 		end
-		next_j = j;
 	    end
 	    CONT: begin
 	        for (x = 0; x <= 15; x = x + 1) begin
@@ -216,8 +234,10 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 		end
 	    end
 	    default: begin
-	        next_i = i;
-		next_j = j;
+	        for (x = 0; x <= 15; x = x + 1) begin
+	            next_i[x] = i[x];
+		    next_j[x] = j[x];
+		end
 	    end
 	endcase
 
@@ -230,6 +250,7 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 
 	// highest cell value logic ------------------------------
 	for (x = 0; x <= 15; x = x + 1) begin
+	if (delay_i[x] != 0) begin
 	    if (high_match[x] >= ins[delay_i[x]][delay_j[x]]) begin
                 if (high_match[x] >= del[delay_i[x]][delay_j[x]]) begin
                     if (high_match[x] >= 0)
@@ -252,23 +273,26 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
                         next_high[x] = 0;
                 end
                 else begin
-                    if (next_del >= 0)
+                    if (del[delay_i[x]][delay_j[x]] >= 0)
                         next_high[x] = del[delay_i[x]][delay_j[x]];
                     else
                         next_high[x] = 0;
                 end
             end
 	end
+	else
+	    next_high[x] = 0;
+	end
 
 	// output logic ----------------------------------
 	// temporary max
 	for (x = 0; x <= 15; x = x + 1) begin
-	    if (next_high[x] > max_tmp[x]) begin
+	    if (next_high[x] > max_tmp[x] && fill_state != WAIT) begin
 	        next_max_tmp      [x] = next_high[x];
 		next_pos_ref_tmp  [x] = delay_i  [x];
-		next_pos_query_tmp[x] = delay_j  [x]
+		next_pos_query_tmp[x] = delay_j  [x];
 	    end
-	    else
+	    else begin
 	        next_max_tmp      [x] = max_tmp      [x];
 		next_pos_ref_tmp  [x] = pos_ref_tmp  [x];
 		next_pos_query_tmp[x] = pos_query_tmp[x];
@@ -330,6 +354,23 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 	    delay_j[x] <= 0;
 	end
 	// cells
+	for (x = 0; x <= 15; x = x + 1) begin
+	    if (i[x] != 0) begin
+	        match[i[x]-1][j[x]-1] <= 0;
+	        ins  [i[x]][j[x]] <= 0;
+	        del  [i[x]][j[x]] <= 0;
+	    end
+	    else begin
+		match[i[x]-1][j[x]-1] <= match[i[x]-1][j[x]-1];
+		ins  [i[x]][j[x]] <= ins  [i[x]][j[x]];
+		del  [i[x]][j[x]] <= del  [i[x]][j[x]];
+	    end
+	    if (delay_i[x] != 0)
+	        high[delay_i[x]][delay_j[x]] = 0;
+	    else
+	        high[delay_i[x]][delay_j[x]] = high[delay_i[x]][delay_j[x]];
+	end
+	/*
 	for (x = 1; x <= 64; x = x + 1) begin
 	    for (y = 1; y <= 48; y = y + 1) begin
 	        match [x-1][y-1] <= 0;
@@ -338,6 +379,7 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 		high  [x][y] <= 0;
 	    end
 	end
+	*/
     end
     else begin // not reset
         fill_state <= next_fill_state;
@@ -375,19 +417,22 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 	    pos_query_tmp[x] <= next_pos_query_tmp[x];
 	end
 	// indices ----------------------------------------
-	i <= next_i;
-	j <= next_j;
-	case(fill_state)
-	    WAIT: begin
-	        delay_i <= delay_i;
-	        delay_j <= delay_j;
-	    end
-	    default: begin // filling
-	        delay_i <= i;
-	        delay_j <= j;
-	    end
-	endcase
+	for (x = 0; x <= 15; x = x + 1) begin
+	    i[x] <= next_i[x];
+	    j[x] <= next_j[x];
+	    case(fill_state)
+	        WAIT: begin
+		    delay_i[x] <= delay_i[x];
+		    delay_j[x] <= delay_j[x];
+	        end
+	        default: begin // filling
+		    delay_i[x] <= i[x];
+		    delay_j[x] <= j[x];
+	        end
+	    endcase
+	end
 	// cells ------------------------------------------
+	/*
 	for (x = 1; x <= 64; x = x + 1) begin
 	    for (y = 1; y <= 48; y = y + 1) begin
 	        for (z = 0; z <= 15; z = z + 1) begin
@@ -409,6 +454,23 @@ module SW #(parameter WIDTH_SCORE = 8, parameter WIDTH_POS_REF = 7, parameter WI
 		        high[x][y] <= high[x][y];
 		end
 	    end
+	end
+	*/
+	for (x = 0; x <= 15; x = x + 1) begin
+	    if (i[x] != 0) begin
+		match[i[x]-1][j[x]-1] <= next_match[x];
+		ins  [i[x]][j[x]] <= next_ins[x];
+		del  [i[x]][j[x]] <= next_del[x];
+	    end
+	    else begin
+		match[i[x]-1][j[x]-1] <= match[i[x]-1][j[x]-1];
+		ins  [i[x]][j[x]] <= ins  [i[x]][j[x]];
+		del  [i[x]][j[x]] <= del  [i[x]][j[x]];
+	    end
+	    if (delay_i[x] != 0)
+	        high[delay_i[x]][delay_j[x]] = next_high[x];
+	    else
+	        high[delay_i[x]][delay_j[x]] = high[delay_i[x]][delay_j[x]];
 	end
     end
     end
